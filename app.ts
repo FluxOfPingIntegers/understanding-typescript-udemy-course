f7045708db3febeb8e305e058cce460224dcf34b
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-04-03 09:06:34 -0400

    moving enum lesson code to new file, objs-arrays-enum.ts

--- app.ts
+++ app.ts
@@ -13,29 +13,3 @@ console.log(combinedAges);
 
 const combinedNames = combine('Max', 'Anna');
 console.log(combinedNames);
-
-
-
-//  enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
-//  
-//  const person = {
-//    name: 'Maximilian',
-//    age: 30,
-//    hobbies: ['Sports', 'Cooking'],
-//    role: Role.ADMIN
-//  };
-//  
-//  
-//  let favoriteActivities: string[];
-//  favoriteActivities = ['Sports'];
-//  
-//  console.log(person.name);
-//  
-//  for (const hobby of person.hobbies) {
-//    console.log(hobby.toLocaleUpperCase());
-//    // console.log(hobby.map()); // !!! ERROR !!!
-//  }
-//  
-//  if (person.role === Role.AUTHOR) {
-//    console.log('is author');
-//  }
