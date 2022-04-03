843f38846e04a3f319cb0f85e829da842a1649fe
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-04-02 23:01:01 -0400

    union types completed

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-var Role;
-(function (Role) {
-    Role["ADMIN"] = "ADMIN";
-    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
-    Role["AUTHOR"] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toLocaleUpperCase());
-    // console.log(hobby.map()); // !!! ERROR !!!
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
+//  enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
+//  
+//  const person = {
+//    name: 'Maximilian',
+//    age: 30,
+//    hobbies: ['Sports', 'Cooking'],
+//    role: Role.ADMIN
+//  };
+//  
+//  
+//  let favoriteActivities: string[];
+//  favoriteActivities = ['Sports'];
+//  
+//  console.log(person.name);
+//  
+//  for (const hobby of person.hobbies) {
+//    console.log(hobby.toLocaleUpperCase());
+//    // console.log(hobby.map()); // !!! ERROR !!!
+//  }
+//  
+//  if (person.role === Role.AUTHOR) {
+//    console.log('is author');
+//  }
--- app.ts
+++ app.ts
@@ -1,23 +1,41 @@
-enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
-
-const person = {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN
-};
-
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toLocaleUpperCase());
-  // console.log(hobby.map()); // !!! ERROR !!!
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-if (person.role === Role.AUTHOR) {
-  console.log('is author');
-}
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
+
+
+
+//  enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
+//  
+//  const person = {
+//    name: 'Maximilian',
+//    age: 30,
+//    hobbies: ['Sports', 'Cooking'],
+//    role: Role.ADMIN
+//  };
+//  
+//  
+//  let favoriteActivities: string[];
+//  favoriteActivities = ['Sports'];
+//  
+//  console.log(person.name);
+//  
+//  for (const hobby of person.hobbies) {
+//    console.log(hobby.toLocaleUpperCase());
+//    // console.log(hobby.map()); // !!! ERROR !!!
+//  }
+//  
+//  if (person.role === Role.AUTHOR) {
+//    console.log('is author');
+//  }
