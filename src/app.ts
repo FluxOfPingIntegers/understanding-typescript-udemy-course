1c24769c9c285e2b2c3d9b5dfeed229f027260f3
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-04-19 14:42:54 -0400

    More on Type Guards - completed

--- src/app.ts
+++ src/app.ts
@@ -24,3 +24,54 @@ type Numeric = number | boolean;
 type Universal = Combinable & Numeric; 
 // because number is present in both above union types, Universal = number
 
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return add.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+  if ('startDate' in emp) {
+    console.log('Privileges: ' + emp.startDate);
+  }
+} // type guard function
+
+printEmployeeInformation({name: 'Manu', startDate: new Date()});
+
+class Car {
+  drive() {
+    console.log('Driving...')
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+} // type guard function
+
+useVehicle(v1);
+useVehicle(v2);
