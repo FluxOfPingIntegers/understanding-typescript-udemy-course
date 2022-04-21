2e9ee2cf503153a2b51867b86ce91500de63e1a2
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-04-21 15:08:28 -0400

    Nullish Coalescing - completed

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 const e1 = {
     name: 'Max',
     privileges: ['create-server'],
@@ -10,52 +11,15 @@ function add(a, b) {
     }
     return a + b;
 }
-function printEmployeeInformation(emp) {
-    console.log('Name: ' + emp.name);
-    if ('privileges' in emp) {
-        console.log('Privileges: ' + emp.privileges);
-    }
-    if ('startDate' in emp) {
-        console.log('Privileges: ' + emp.startDate);
-    }
-}
-printEmployeeInformation({ name: 'Manu', startDate: new Date() });
-class Car {
-    drive() {
-        console.log('Driving...');
-    }
-}
-class Truck {
-    drive() {
-        console.log('Driving a truck...');
-    }
-    loadCargo(amount) {
-        console.log('Loading cargo...' + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log('Moving at speed: ' + speed);
-}
-moveAnimal({ type: 'bird', flyingSpeed: 10 });
-const userInputElement = document.getElementById('user-input');
-userInputElement.value = 'Hi there!';
+const result = add('Max', ' Schwarz');
+result.split(' ');
+const fetchedUserData = {
+    id: 'u1',
+    name: 'Max',
+    job: { title: 'CEO', description: 'My own company' }
+};
+console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
+const userInput = undefined;
+const storedData = userInput !== null && userInput !== void 0 ? userInput : 'DEFAULT';
+console.log(storedData);
 //# sourceMappingURL=app.js.map
