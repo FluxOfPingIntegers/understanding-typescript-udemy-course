99ce8f6d77ef3bd35994b068f25c77f80587de8e
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-04-21 14:43:34 -0400

    Index Properties - completed

--- src/app.ts
+++ src/app.ts
@@ -104,5 +104,17 @@ function moveAnimal(animal: Animal) {
 moveAnimal({type: 'bird', flyingSpeed: 10});
 
 // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
-const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
-userInputElement.value = 'Hi there!';
+const userInputElement = document.getElementById('user-input');
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'Hi there!';
+}
+
+interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with a character!'}
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email!',
+  username: 'Must start with a capital character!'
+}
