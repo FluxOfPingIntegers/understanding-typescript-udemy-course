704372703c6bda4a14a61c8954e78f67778a88e0
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-04-23 20:36:30 -0400

    Creating a Generic Function - completed

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,9 @@
 //   data.split(' ');
 // })
 
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 })
+console.log(mergedObj)
