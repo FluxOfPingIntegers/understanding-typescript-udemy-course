cbf32ee0329159cbae55e1c600ee27cdb48d3dae
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-04-23 20:26:04 -0400

    Built-in Generics & What are Generics? - completed

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-const names: Array<string> = []; // string[] , is a generic type (type connected to other type)
+// const names: Array<string> = []; // string[] , is a generic type (type connected to other type)
+// names[0].split(' ');
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!');
+//   }, 2000);
+// })
+// 
+// promise.then(data => {
+//   data.split(' ');
+// })
+
