486ba29bc7445dc2080610ed09c4db7624fd4f04
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-04-27 14:02:11 -0400

    starting Product class

--- src/app.ts
+++ src/app.ts
@@ -33,3 +33,27 @@ class Person {
 const pers = new Person()
 
 console.log(pers);
+
+// ---
+
+class Product {
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - should be positive!');
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax)
+  }
+}
