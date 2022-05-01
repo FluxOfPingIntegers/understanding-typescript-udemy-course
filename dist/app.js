e46f6d84b661db86d1ca44c1d466db88890d51d6
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-05-01 13:10:19 -0400

    Interacting with DOM Elements - completed

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,7 @@ class ProjectInput {
     constructor(args, temp = args.temp, host = args.host) {
         const tempEl = document.getElementById(temp);
         const hostEl = document.getElementById(host);
-        if ((tempEl instanceof HTMLTemplateElement) && hostEl instanceof HTMLDivElement) {
+        if (tempEl instanceof HTMLTemplateElement && hostEl instanceof HTMLDivElement) {
             this.templateElement = tempEl;
             this.hostElement = hostEl;
         }
@@ -12,7 +12,19 @@ class ProjectInput {
         }
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
+        this.configure();
         this.attach();
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor(args: {temp: string, host: string}, temp = args.temp, host = args.host) {
     const tempEl = document.getElementById(temp)
     const hostEl = document.getElementById(host)
 
-    if ( (tempEl instanceof HTMLTemplateElement) && hostEl instanceof HTMLDivElement ) {
+    if ( tempEl instanceof HTMLTemplateElement && hostEl instanceof HTMLDivElement ) {
       this.templateElement = tempEl;
       this.hostElement = hostEl;
     } else {
@@ -16,7 +19,23 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
