30f2c8686bbe9eb3c22897126c1af45524b40d6e
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-05-10 16:02:42 -0400

    Using ES Modules - completed

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,6 @@
-"use strict";
+import { ProjectInput } from './components/project-input.js';
+import { ProjectList } from './components/project-list.js';
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
 //# sourceMappingURL=app.js.map
