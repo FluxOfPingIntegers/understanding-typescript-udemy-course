ca13807127ade56c0c72e2ef9aa6404c124eca0f
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-05-13 11:47:43 -0400

    Finishing the Setup & Adding webpack-dev-server - completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,54 @@
+const path = require('path')
+ 
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  performance: {
+    hints: false,
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve( __dirname, 'dist' ),
+    // clear: true
+    publicPath: '/dist/'   
+  },
+  devtool: 'inline-source-map',  
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, 
+        use: 'ts-loader', 
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    static:  {
+      directory: path.resolve( __dirname, './' ),
+    },
+    compress: true,
+    port: 3000
+  },
+  resolve: {  
+    extensions: [
+      '.ts',
+      '.js'
+    ]
+  }
+};
+
+/*
+-----WEBPACK.CONFIG.JS AS PER LESSON RESOURCES (old)-----
+
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.ts',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
   },
   devtool: 'inline-source-map',
   module: {
@@ -18,6 +62,7 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js']
-  },
-  mode: 'development'
+  }
 };
+
+*/
