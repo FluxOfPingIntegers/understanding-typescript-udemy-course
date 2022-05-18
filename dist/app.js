361a3604550524bc4d256daa4b159933f8207a14
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-05-18 16:02:04 -0400

    No Types Needed class-transformer - completed

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,12 @@
 import 'reflect-metadata';
 import { plainToClass } from '../node_modules/class-transformer/index';
-
 import { Product } from "./product.model";
-
 const products = [
-  {title: 'A Carpet', price: 29.99}, 
-  {title: 'A Book', price: 10.99 }
+    { title: 'A Carpet', price: 29.99 },
+    { title: 'A Book', price: 10.99 }
 ];
-
-// const p1 = new Product('A Book', 12.99);
-
 const loadedProducts = plainToClass(Product, products);
-
 for (const prod of loadedProducts) {
-  console.log(prod.getInformation());
+    console.log(prod.getInformation());
 }
+//# sourceMappingURL=app.js.map
