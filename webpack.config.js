6150d883e55be9e4c4f48ed8823469c4d11abeee
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-05-22 09:33:58 -0400

    Setting Up a Google API Key - completed (hidden)

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
-const path = require('path')
+const path = require('path'); // a variable provided by a node.js method representing the absolute path
+const Dotenv = require('dotenv-webpack'); // third party package to hide environment variables in webpack
  
-module.exports = {
+module.exports = { // the object webpack will look for within webpack.config.js with your webpack settings
   mode: 'development',
-  entry: './src/app.ts',
+  entry: './src/app.ts', // entry point webpack looks at first before proceeding to imports
   performance: {
     hints: false,
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve( __dirname, 'dist' ),
-    // clear: true
-    publicPath: '/dist/'   
+    filename: 'bundle.js', // what you will name the resulting bundled javascript from webpack
+    path: path.resolve( __dirname, 'dist' ), // folder on disk where bundled js should go
+    clean: true,  // clears contents of dist folder prior to each build
+    publicPath: '/dist/' // where javascript files should be accessed by the browser
   },
-  devtool: 'inline-source-map',  
+  devtool: 'inline-source-map', // allows you to debug/view .js and .ts files in the browser
   module: {
     rules: [
       {
-        test: /\.ts$/, 
-        use: 'ts-loader', 
-        exclude: /node_modules/
+        test: /\.ts$/, // look for files ending in .ts
+        use: 'ts-loader', // what webpack will use to translate typescript to javascript
+        exclude: /node_modules/ // do not include files within the node_modules folder
       }
     ]
   },
@@ -28,14 +29,20 @@ module.exports = {
       directory: path.resolve( __dirname, './' ),
     },
     compress: true,
-    port: 3000
+    port: 3000 // the port webpack will use to serve your webpage
   },
   resolve: {  
     extensions: [
       '.ts',
       '.js'
     ]
-  }
+  },
+  plugins: [
+    new Dotenv({
+      path: './.env',
+      safe: true,
+    })
+  ]
 };
 
 /*
