e9083c24238e1385f273d4da0efaa1d094ac8b3a
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-05-22 09:44:04 -0400

    Using Axios to Fetch Coordinates for an Entered Address - completed

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,26 @@ const addressInput = document.getElementById('address')! as HTMLInputElement;
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
   
-  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+  axios.get<GoogleGeocodingResponse>(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
     enteredAddress
     )}&key=${GOOGLE_API_KEY}`
   ).then(response => {
-    console.log(response);
+    if (response.data.status !== 'OK') {
+      throw new Error('Could not fetch location!');
+    }
+    const coordinates = response.data.results[0].geometry.location;
   }).catch(err => {
+    alert(err.message);
     console.log(err);
   });
 }
