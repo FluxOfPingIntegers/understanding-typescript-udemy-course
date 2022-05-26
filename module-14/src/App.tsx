0c559e7d4dfda3c26efb5bab579603d2e0704425
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-05-26 11:44:29 -0400

    Cross-Component Communication - completed

--- module-14/src/App.tsx
+++ module-14/src/App.tsx
@@ -5,12 +5,17 @@ import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
   const todos = [{id: 't1', text: 'Finish the course!'}];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
-      {<NewTodo />}
+      {<NewTodo onAddTodo={todoAddHandler} />}
       <TodoList items={todos} />
     </div>
   );
-}
+};
 
 export default App;
